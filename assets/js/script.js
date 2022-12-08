06c39f538463e0efd7bff098d4c037e0f2a87eb2
andynewen <117054251+andynewen@users.noreply.github.com>
2022-12-08 17:30:22 +1100

    displayed questions, answers and timer js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
+//Time and score
 let timeEl = document.querySelector("p.time");
 let secondsLeft = 75;
 let scoreEl = document.querySelector("#score");
 
+//Intro section
 const introEl = document.querySelector("#intro");
 
+//Question section
 const questionsEl = document.querySelector("#questions");
 let questionEl = document.querySelector("#question");
 let questionCount = 0;
 const yesnoEl = document.querySelector("#yesno");
 
 
-
+//Button to start the quiz with timer and display questions
 const startBtn = document.querySelector("#start");
 startBtn.addEventListener("click", startQuiz);
 function startQuiz() {
@@ -18,5 +21,73 @@ function startQuiz() {
     questionsEl.style.display = "block";
     questionCount = 0;
 
+    setTime();
+    setQuestion(questionCount);
 }
 
+//Timer for quiz
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+            questionsEl.style.display = "none";
+            finalEl.style.display = "block";
+            scoreEl.textContent = secondsLeft;
+        }
+    }, 1000);
+}
+
+//All buttons within the page
+const ansBtn = document.querySelectorAll("button.ansBtn")
+const ans1Btn = document.querySelector("#answer1");
+const ans2Btn = document.querySelector("#answer2");
+const ans3Btn = document.querySelector("#answer3");
+const ans4Btn = document.querySelector("#answer4");
+
+//Object for questions, answers and correct answers
+const questions = [ // array of objects
+    {
+        // Question 0
+        question: "Commonly used data types do NOT include:",
+        answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+        correctAnswer: "2"
+    },
+    {
+        // Question 1
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answers: ["1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets"],
+        correctAnswer: "1"
+    },
+    {
+        // Question 2
+        question: "Arrays in Javascript can be used to store ____.",
+        answers: ["1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the Above"],
+        correctAnswer: "3"
+    },
+    {
+        // Question 3
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["1. Commmas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        // Question 4
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. Javascript", "2. Terminal/Bash", "3. For Loops", "4. console.log"],
+        correctAnswer: "3"
+    }
+];
+
+//Function to set the questions 
+function setQuestion(id) {
+    if (id < questions.length) {
+        questionEl.textContent = questions[id].question;
+        ans1Btn.textContent = questions[id].answers[0];
+        ans2Btn.textContent = questions[id].answers[1];
+        ans3Btn.textContent = questions[id].answers[2];
+        ans4Btn.textContent = questions[id].answers[3];
+    }
+}
